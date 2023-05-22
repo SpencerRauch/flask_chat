9a37a874d0e466c4bd9b6707b4d97c042a226ffd
Spencer Rauch <rauchs@gmail.com>
2023-05-22 10:37:41 -0700

    more comments

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask_app import app, socketio
 from flask_app.controllers import users_controller, chat_controller, rooms_controller
 
-
+# notice we use the socketio instance to run our app
 if __name__=='__main__':
     socketio.run(app,debug=True)
